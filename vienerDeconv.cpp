6e6de380e766e732107d0aef1f928b7c7ad9af84
tetris0k <tyoma_mur@mail.ru>
2016-07-20 01:20:05 +0300

    Repair blur_edge. Now it works. Add memory cleaning. Add the right image reading and preprocessing.

--- vienerDeconv.cpp
+++ vienerDeconv.cpp
@@ -19,30 +19,44 @@ cv::Mat motion_kernel(int angle, int d, int sz)
 
 int main(int argc, char** argv)
 {
-	char* filename = "____________";
+	char* filename = "____!!!!____";						//input some file!!!
 	int angle = 135;
 	int d = 31;
 	int sz = 65;
 
 	cv::Mat img;
 
-	//preparing an image for further processing
-	img = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
-	img.convertTo(img, CV_32F);
+	img = cv::imread(filename, CV_LOAD_IMAGE_GRAYSCALE);	//reading and preparing an image for 
+	img.convertTo(img, CV_32FC1);							//further processing. grayscale - important! (1 channel)
 	cv::divide(img, 255.0, img);
 
-	cv::namedWindow("initial", cv::WINDOW_AUTOSIZE);
+	
+	cv::namedWindow("initial", cv::WINDOW_AUTOSIZE);		//windows creation
 	cv::namedWindow("blurred", cv::WINDOW_AUTOSIZE);
 	cv::namedWindow("PSF", cv::WINDOW_NORMAL);
+
+															//initial creation of blurred image (border blur)
+	cv::Mat blurred = cv::Mat::zeros(img.rows, img.cols, CV_32FC1);
+
+	int d = 25;												//border blur radious
+	
+	blur_edge(img, blurred, d);								//border blur
+
 	cv::Mat psf;
+	
+	psf = motion_kernel(angle, d, sz);						//motion psf
 
+	cv::imshow("PSF", psf);									//showing the images
+	cv::imshow("blurred", blurred);
 
-	psf = motion_kernel(angle, d, sz);
-
-	cv::imshow("PSF", psf);
 	cv::waitKey(0);
-	cv::destroyAllWindows();
-	return 15;
+
+	img.release();											//memory cleaning
+	blurred.release();
+	psf.release();
+
+	cv::destroyAllWindows();								//exit
+	return 0;
 }
 
 void blur_edge(cv::Mat img, cv::Mat blurred, int d){
@@ -50,32 +64,36 @@ void blur_edge(cv::Mat img, cv::Mat blurred, int d){
 		d = 31;
 	int height = img.rows;
 	int width = img.cols;
-	cv::Mat img_pad(height + 2 * d, width + 2 * d, CV_32F);
-	cv::Mat img_blur(height + 2 * d, width + 2 * d, CV_32F);
+	cv::Mat img_pad(height + 2 * d, width + 2 * d, CV_32FC1);
+	cv::Mat img_blur(height + 2 * d, width + 2 * d, CV_32FC1);
 	cv::copyMakeBorder(img, img_pad, d, d, d, d, cv::BORDER_WRAP);
 	cv::GaussianBlur(img_pad, img_blur, cvSize(2 * d + 1, 2 * d + 1), -1);
 	img_blur = img_blur(cv::Range(d, height + d), cv::Range(d, width + d));
-	cv::Mat prefinal(height, width, CV_32F);
-	cv::Mat anti_pref(height, width, CV_32F);
+	cv::Mat blur_mat(height, width, CV_32FC1);
+	cv::Mat inv_blur_mat(height, width, CV_32FC1);
 	int arr[4];
 	float tmp;
-	for (int i = 0; i < height; i++){
-		for (int j = 0; j < width; j++){
+	for (int i = 0; i < blurred.rows; i++){
+		for (int j = 0; j < blurred.cols; j++){
 			arr[0] = i; arr[1] = height - 1 - i; arr[2] = j; arr[3] = width - 1 - j;
 			tmp = (float)(Min(arr, 4)) / d;
 			tmp = (tmp < 1.0) ? tmp : (float)(1.0);
-			prefinal.at<float>(i, j) = tmp;
-			anti_pref.at<float>(i, j) = (float)(1.0) - tmp;
-			blurred.at<float>(i, j) = img.at<float>(i, j) * tmp + img_blur.at<float>(i, width - j - 1) * ((float)(1.0) - tmp);
+			blur_mat.at<float>(i, j) = tmp;
+			inv_blur_mat.at<float>(i, j) = (float)(1.0) - tmp;
 		}
 	}
-	//	cv::multiply(img, prefinal, blurred);
-	//	cv::multiply(img_blur, anti_pref, prefinal);
-	//	cv::add(blurred, prefinal, blurred);
+	try{
+		cv::multiply(img, blur_mat, blurred);
+		cv::multiply(img_blur, inv_blur_mat, blur_mat);
+		cv::add(blurred, blur_mat, blurred);
+	}
+	catch (cv::Exception const& e) {
+		std::cerr << "OpenCV exception at blur_edge: " << e.what() << std::endl;
+	}
 	img_pad.release();
 	img_blur.release();
-	prefinal.release();
-	anti_pref.release();
+	blur_mat.release();
+	inv_blur_mat.release();
 }
 
 int Min(int* arr, int len){
