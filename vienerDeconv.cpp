d1dc1b154874551fe435a3acb3ac929297f7f731
tetris0k <tyoma_mur@mail.ru>
2016-07-19 21:54:55 +0300

    Add blur_edge function without prototype.

--- vienerDeconv.cpp
+++ vienerDeconv.cpp
@@ -19,12 +19,23 @@ cv::Mat motion_kernel(int angle, int d, int sz)
 
 int main(int argc, char** argv)
 {
+	char* filename = "____________";
 	int angle = 135;
 	int d = 31;
 	int sz = 65;
 
+	cv::Mat img;
+
+	//preparing an image for further processing
+	img = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
+	img.convertTo(img, CV_32F);
+	cv::divide(img, 255.0, img);
+
+	cv::namedWindow("initial", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("blurred", cv::WINDOW_AUTOSIZE);
 	cv::namedWindow("PSF", cv::WINDOW_NORMAL);
 	cv::Mat psf;
+
 
 	psf = motion_kernel(angle, d, sz);
 
@@ -32,4 +43,37 @@ int main(int argc, char** argv)
 	cv::waitKey(0);
 	cv::destroyAllWindows();
 	return 15;
+}
+
+void blur_edge(cv::Mat img, cv::Mat blurred, int d){
+	if (!d)
+		d = 31;
+	int height = img.rows;
+	int width = img.cols;
+	cv::Mat img_pad(height + 2 * d, width + 2 * d, CV_32F);
+	cv::Mat img_blur(height + 2 * d, width + 2 * d, CV_32F);
+	cv::copyMakeBorder(img, img_pad, d, d, d, d, cv::BORDER_WRAP);
+	cv::GaussianBlur(img_pad, img_blur, cvSize(2 * d + 1, 2 * d + 1), -1);
+	img_blur = img_blur(cv::Range(d, height + d), cv::Range(d, width + d));
+	cv::Mat prefinal(height, width, CV_32F);
+	cv::Mat anti_pref(height, width, CV_32F);
+	int arr[4];
+	float tmp;
+	for (int i = 0; i < height; i++){
+		for (int j = 0; j < width; j++){
+			arr[0] = i; arr[1] = height - 1 - i; arr[2] = j; arr[3] = width - 1 - j;
+			tmp = (float)(Min(arr, 4)) / d;
+			tmp = (tmp < 1.0) ? tmp : (float)(1.0);
+			prefinal.at<float>(i, j) = tmp;
+			anti_pref.at<float>(i, j) = (float)(1.0) - tmp;
+			blurred.at<float>(i, j) = img.at<float>(i, j) * tmp + img_blur.at<float>(i, width - j - 1) * ((float)(1.0) - tmp);
+		}
+	}
+	//	cv::multiply(img, prefinal, blurred);
+	//	cv::multiply(img_blur, anti_pref, prefinal);
+	//	cv::add(blurred, prefinal, blurred);
+	img_pad.release();
+	img_blur.release();
+	prefinal.release();
+	anti_pref.release();
 }
