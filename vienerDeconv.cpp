42cb71f9a036c71e5d6a1d5a19bf66a9b217949b
mihailsharabin <mihailsharabin@gmail.com>
2016-07-20 18:20:44 +0300

    Update vienerDeconv.cpp

--- vienerDeconv.cpp
+++ vienerDeconv.cpp
@@ -1,19 +1,13 @@
 #include "vienerDeconv.h"
 
-cv::Mat motion_kernel(int angle, int d, int sz)
-{
-	cv::Mat kern(1, d, CV_32F, cv::Scalar(1));
-	double c = cos(angle*Pi / 180);
-	double s = sin(angle*Pi / 180);
-	float A[2][3] = {{c, -s, 0}, {s, c, 0}};
-	int sz2 = sz / 2;
-	A[0][2] = sz2 - A[0][0] * (d - 1)*0.5;
-	A[1][2] = sz2 - A[1][0] * (d - 1)*0.5;
-	cv::Mat M(2, 3, CV_32F, A);
-
-	cv::Mat tmp(d, d, CV_32F);
-	cv::Size si(d, d);
-	cv::warpAffine(kern, tmp, M, si, CV_INTER_LINEAR);
+cv::Mat motion_kernel(int angle, int d, int sz){
+	cv::Mat kern = cv::Mat::ones(1, d, CV_32FC1);
+	cv::Mat tmp = cv::Mat::zeros(d, d, CV_32FC1);
+	cv::Mat matr = (cv::Mat_<double>(2, 3) << 1, 0, 0, 0, 1, d / 2);
+	cv::warpAffine(kern, tmp, matr, tmp.size());
+	cv::Point2f center(d / 2,  d / 2);
+	cv::Mat M = cv::getRotationMatrix2D(center, angle, 1.0);
+	cv::warpAffine(tmp, tmp, M, tmp.size());
 	return tmp;
 }
 
@@ -37,7 +31,7 @@ int main(int argc, char** argv)
 	
 	cv::namedWindow("initial", cv::WINDOW_AUTOSIZE);		//windows creation
 	cv::namedWindow("blurred", cv::WINDOW_AUTOSIZE);
-	cv::namedWindow("PSF", cv::WINDOW_NORMAL);
+	cv::namedWindow("psf", cv::WINDOW_NORMAL);
 	cv::namedWindow("defocus_psf", cv::WINDOW_NORMAL);
 //	cv::namedWindow("deconvolved", cv::WINDOW_AUTOSIZE);
 															//initial creation of blurred image (border blur)
@@ -54,7 +48,7 @@ int main(int argc, char** argv)
 	psf = motion_kernel(angle, d, sz);						//motion psf
 
 	cv::imshow("initial", img);
-	cv::imshow("PSF", psf);									//showing the images
+	cv::imshow("psf", psf);									//showing the images
 	cv::imshow("blurred", blurred);
 	cv::imshow("defocus_psf", defocus_psf);
 //	cv::imshow("deconvolved", deconvolved);
